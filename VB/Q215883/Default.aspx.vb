a318af919e4b9227db01a4f9733e039327ccb60f
Anton Sarmin <anton.sarmin@devexpress.com>
2021-03-23 16:43:30 +0300

    vb fix

--- VB/Q215883/Default.aspx.vb
+++ VB/Q215883/Default.aspx.vb
@@ -11,43 +11,46 @@ Imports System.Web.UI.HtmlControls
 Imports DevExpress.Web.ASPxGridView
 Imports DevExpress.Xpo
 Imports DevExpress.Data.Filtering
+Imports DevExpress.Web
 
 Partial Public Class _Default
-	Inherits System.Web.UI.Page
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-		Dim fSession As New Session()
-		Dim gv As New ASPxGridView()
-		gv.DataSource = New XPCollection(Of Customer)(fSession)
-		gv.SettingsDetail.ShowDetailRow = True
-		gv.SettingsDetail.ShowDetailButtons = True
-		gv.Templates.DetailRow = New DetailRowTemplate(fSession)
-		gv.KeyFieldName = "Oid"
-		ASPxRoundPanel1.Controls.Add(gv)
-		gv.DataBind()
-	End Sub
+    Inherits System.Web.UI.Page
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        Dim fSession As New Session()
+        Dim gv As New ASPxGridView()
+        gv.ID = "gridMaster"
+        gv.DataSource = New XPCollection(Of Customer)(fSession)
+        gv.SettingsDetail.ShowDetailRow = True
+        gv.SettingsDetail.ShowDetailButtons = True
+        gv.Templates.DetailRow = New DetailRowTemplate(fSession)
+        gv.KeyFieldName = "Oid"
+        ASPxRoundPanel1.Controls.Add(gv)
+        gv.DataBind()
+    End Sub
 End Class
 
 Public Class DetailRowTemplate
-	Implements ITemplate
-	Private fSession As Session
+    Implements ITemplate
+    Private fSession As Session
 
-	Public Sub New(ByVal s As Session)
-		fSession = s
-	End Sub
-	Public Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
-		Dim detailGridView As New ASPxGridView()
-		detailGridView.SettingsDetail.IsDetailGrid = True
-		detailGridView.KeyFieldName = "Oid"
-		AddHandler detailGridView.BeforePerformDataSelect, AddressOf OnDetailViewBeforePerformDataSelect
-		container.Controls.Add(detailGridView)
-	End Sub
+    Public Sub New(ByVal s As Session)
+        fSession = s
+    End Sub
+    Public Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
+        Dim detailGridView As New ASPxGridView()
+        detailGridView.ID = "gridDetail"
+        detailGridView.SettingsDetail.IsDetailGrid = True
+        detailGridView.KeyFieldName = "Oid"
+        AddHandler detailGridView.BeforePerformDataSelect, AddressOf OnDetailViewBeforePerformDataSelect
+        container.Controls.Add(detailGridView)
+    End Sub
 
-	Protected Sub OnDetailViewBeforePerformDataSelect(ByVal sender As Object, ByVal e As EventArgs)
-		Dim grid As ASPxGridView = CType(sender, ASPxGridView)
-		Dim key As Object = grid.GetMasterRowKeyValue()
-		Dim customer As Customer = fSession.GetObjectByKey(Of Customer)(key)
-		Dim dataSource As New XPCollection(Of Order)(customer.Orders)
-		dataSource.DisplayableProperties = "Oid;OrderName;OrderDate"
-		grid.DataSource = dataSource
-	End Sub
+    Protected Sub OnDetailViewBeforePerformDataSelect(ByVal sender As Object, ByVal e As EventArgs)
+        Dim grid As ASPxGridView = CType(sender, ASPxGridView)
+        Dim key As Object = grid.GetMasterRowKeyValue()
+        Dim customer As Customer = fSession.GetObjectByKey(Of Customer)(key)
+        Dim dataSource As New XPCollection(Of Order)(customer.Orders)
+        dataSource.DisplayableProperties = "Oid;OrderName;OrderDate"
+        grid.DataSource = dataSource
+    End Sub
 End Class
